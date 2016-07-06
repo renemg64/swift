6395d43fd2d6650a3cb2bcea4ec545b49ff6c4e1
PC-RMG <renemg64@gmail.com>
2016-07-06 17:06:43 -0500

    de 0 a 100

---
+++ de0a100.playground/section-1.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+for i in 0...100 {
+    if i%5 == 0 {
+        print("\n\t#\(i)Bingo")
+    }
+    	if i%2 == 0 {
+        print("\n\t#\(i)par")
+    }
+    else{
+        print("\n\t#\(i)impar")
+    }
+    if i>30 && i<40 {
+        print("\n\t#\(i)Viva Swift!!!")
+    }
+    
+    
+}
